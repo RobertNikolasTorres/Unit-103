d638b9ebcca834ef84fb07bd2abae8cf8656aa08
robert <robert.nikolas.torres@gmail.com>
2025-07-26 01:03:15 -0400

    103 - Assignment 2

---
+++ Practice/AssignmentPractice.swift
@@ -0,0 +1,18 @@
+//
+//  AssignmentPractice.swift
+//  Unit 103
+//
+//  Created by Robert Nikolas Torres on 7/25/25.
+//
+
+import SwiftUI
+
+struct AssignmentPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AssignmentPractice()
+}
